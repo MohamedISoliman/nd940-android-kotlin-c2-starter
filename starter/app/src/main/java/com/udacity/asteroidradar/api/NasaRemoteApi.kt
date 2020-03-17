670631c5f93b2413dbac14d5b2c5d43a5cc2b9ee
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-17 21:53:32 +0300

    refactor packages

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NasaRemoteApi.kt
@@ -0,0 +1,6 @@
+package com.udacity.asteroidradar.api
+
+/**
+ *
+ * Created by Mohamed Ibrahim on 3/17/20.
+ */
---
+++ starter/app/src/main/java/com/udacity/asteroidradar/domain/AsteroidListFetcher.kt
@@ -0,0 +1,6 @@
+package com.udacity.asteroidradar.domain
+
+/**
+ *
+ * Created by Mohamed Ibrahim on 3/17/20.
+ */
